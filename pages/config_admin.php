1fa11e7ba005997d8a53d783cad6f382df2aca1c
BoC8 <clemclemblmblm@gmail.com>
2025-11-12 08:57:05 +0100

    Update config_admin.php

--- pages/config_admin.php
+++ pages/config_admin.php
@@ -1,4 +1,4 @@
 <?php
 // Tu mets ton email et le hash copié à l’étape 1
 $ADMIN_EMAIL = 'admin@gjpb.com';
-$ADMIN_HASH  = '$2y$10$9iWcbC1HFGZXTapyaju88.b/X.j6p8MmnkG/3mNJNS5QQAxq.UX9a';
+$ADMIN_HASH  = '$2y$10$TrgP99U6lmNPQjX2xh9TSubBKnXXLgeZKt3ZTkQuhm3xYfr2yLn6q';
